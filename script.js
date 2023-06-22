993257239a001d26e63f4daf2e799248c7aa7bfa
Akib Mulla <70190643+ItzzAkib07@users.noreply.github.com>
2023-06-22 12:45:16 +0530

    Update script.js
    removed the validation function.

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-function validate(event) {
-  event.preventDefault(); // Prevent form submission if validation fails
-
-  const nameInput = document.getElementById("name");
-  const emailInput = document.getElementById("email");
-  const projectInput = document.getElementById("project");
-  const messageInput = document.getElementById("message");
-
-  if (nameInput.value.trim() === "" || emailInput.value.trim() === "" || projectInput.value.trim() === "" || messageInput.value.trim() === "" ) {
-    // Empty field(s) found, show toast message
-    showToast("Please fill in all fields");
-  } else {
-    // Form is valid, submit it
-    showToast("Thank you for connecting. \n I will get back to you shortly...");
-    document.getElementById("myForm").submit();
-  }
-}
-
-function showToast(message) {
-  // Display toast message using a library like Toastify
-  Toastify({
-    text: message,
-    duration: 3000,
-    close: true,
-    stopOnFocus: true,
-    gravity: "top", // Display at the top of the page
-    position: "right", // Center horizontally
-    backgroundColor: "linear-gradient(to right, #ff5f6d, #ffc371)", // Custom background color
-    style: {
-              width: "30rem"
-            },
-  }).showToast();
-}
-
-
-
 // toggle Menu and Scroll Top Button
 $(document).ready(function () {
 
